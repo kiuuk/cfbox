1ce8fc81c39311ebad97f6040066efaa30083c3f
Kay Ha <kiwook.ha@gmail.com>
2019-02-24 00:10:28 +0900

    set score select

--- app/javascript/packs/views/wods/new.vue
+++ app/javascript/packs/views/wods/new.vue
@@ -9,30 +9,29 @@
       </div>
       <!-- score -->
       <div class="row">
-        <div class="col-md-5 mb-3">
+        <div class="col-12 mb-3">
           <label for="score">Score</label>
-          <select class="custom-select d-block w-100" id="score" required>
-            <option value>Choose...</option>
-            <option>For Time</option>
-            <option>AMRAP</option>
+          <select v-model="score_select" class="custom-select d-block w-100" id="score">
+            <option :value="null" disabled selected>Select Score</option>
+            <option v-for="score in scores" :key="score.value" :value="score.value">{{score.text}}</option>
           </select>
         </div>
-        <div class="col-md-5 mb-3">
-          <label for="score_set">Time set</label>
+        <div class="col-12 mb-3">
+          <label for="score_set">Set timer</label>
           <input type="text" class="form-control" id="score_set" placeholder="minutes">
         </div>
       </div>
       <!-- rep -->
       <div class="row">
-        <div class="col-md-5 mb-3">
+        <div class="col-12 mb-3">
           <label for="rep">Rep</label>
-          <select class="custom-select d-block w-100" id="rep" required>
+          <select class="custom-select d-block w-100" id="rep">
             <option value>Choose...</option>
             <option>Each rep</option>
             <option>Routine rep</option>
           </select>
         </div>
-        <div class="col-md-5 mb-3">
+        <div class="col-12 mb-3">
           <label for="rep_set">Routine set</label>
           <input type="text" class="form-control" id="rep_set" placeholder="1-2-3-4-5">
         </div>
@@ -41,8 +40,8 @@
       <div class="mb-3">
         <label for="movement_1">Movements</label>
         <div class="d-flex">
-          <input type="number" class="form-control mr-2">
-          <input type="text" class="form-control" id="movement_1">
+          <input type="number" class="form-control mr-2 w-25" placeholder="rep">
+          <input type="text" class="form-control w-75" id="movement_1">
         </div>
       </div>
       <!-- desc -->
@@ -57,7 +56,18 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      score_select: null,
+      scores: [
+        { text: "For Time", value: 0 },
+        { text: "As Many Rep As Possible", value: 1 },
+        { text: "Every Minute on the Minute", value: 2 }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
